6d9e20484ca74a57e02ba1bc85b28c915d48145f
SaifPanjeshah <saifushah70@gmail.com>
2022-05-30 21:34:59 +0530

    Code Changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 function App() {
-  const[cartIsShown,setCartIsShown] = useState(false);
-
-  function showCartHandler() {
-    setCartIsShown(true);
-  }
-  function hideCartHandler() {
-    setCartIsShown(false);
-  }
   return (
     <CartProvider>
-     <Cart /> 
+     <Cart />
      <Header />
       <main>
         <Meals />
