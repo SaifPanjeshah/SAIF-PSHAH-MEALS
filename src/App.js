3631f13492909eff7d055108bf4590c6a68214fc
SaifPanjeshah <saifushah70@gmail.com>
2022-05-30 21:29:13 +0530

    App.js added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-
+import Cart from './components/Cart/Cart';
 function App() {
   const[cartIsShown,setCartIsShown] = useState(false);
 
@@ -15,8 +15,8 @@ function App() {
   }
   return (
     <CartProvider>
-      {cartIsShown &&<div>Cart...</div>}
-      <Header onShowCart={showCartHandler}/>
+     <Cart /> 
+     <Header />
       <main>
         <Meals />
       </main>
